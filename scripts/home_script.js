aa48734a6d0563e51f9753103df1075e37fd6c14
Dexter Pacaña <dextepacanya@gmail.com>
2025-08-19 22:22:10 +0800

    Update home_script.js

--- scripts/home_script.js
+++ scripts/home_script.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {//mogana ra'g mahuman ug load ang DOM
 
-  const logoutBtn = document.getElementById('logoutBtn');
-  logoutBtn.addEventListener('click', () => {
-	const confirmLogout = confirm("Do you want to log out?");
+  const logoutBtn = document.getElementById('logoutBtn');//pislitanan sa logout
+  logoutBtn.addEventListener('click', () => {//function inig logout
+	const confirmLogout = confirm("Do you want to log out?");//mangutana sa daan sa dili pa mo hawa
 	
-	if (confirmLogout) {
-	  setTimeout(() => {
-	    window.location.href = "index.html";
+	if (confirmLogout) {//kumpermasyon sa pag logout
+	  setTimeout(() => {//kadyut pa aysa mo hawa
+	    window.location.href = "index.html";//balik sa login page
 	  }, 1000);
 	}
   });
+
 });
