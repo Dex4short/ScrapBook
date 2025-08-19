3438b33da4f81fc641f391463c384c1ac9a65487
Dexter Pacaña <dextepacanya@gmail.com>
2025-08-19 22:41:22 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
+document.getElementById("loginForm").addEventListener("submit", function(e) {//mugana ni siya inig pislit sa login
   e.preventDefault(); 
 
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const message = document.getElementById("message");
+  const username = document.getElementById("username").value;//input sa username
+  const password = document.getElementById("password").value;//input sa password
+  const message = document.getElementById("message");//output kong ok ang passwrod or dili
 
-  if (username === "Dexter" && password === "12345") {
+  if (username === "Dexter" && password === "12345") {//username ug password nga dawatun
     message.style.color = "green";
     message.textContent = "Login successful!";
 	
-	setTimeout(() => {
-	    window.location.href = "home.html";
+	setTimeout(() => {//kadyut sa ayha mo sulod sa scrapbook home page
+	    window.location.href = "home.html";//pag balhin sa home page
 	}, 1000);
 	
   } else {
@@ -18,3 +18,4 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     message.textContent = "Invalid username or password.";
   }
 });
+
